de3bfecbf266be095d6bca4a944721f8cd9842e2
Nick Talledo <nicolastalledo13@gmail.com>
2023-07-13 15:30:12 -0400

    fix server

--- models/recipe.js
+++ models/recipe.js
@@ -1,52 +1,49 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// const sequelize = require('../config/config');
 
-class Recipe extends Model {}
-
-Recipe.init(
-  {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: [0, 500], // Maximum of 500 characters
-      },
-    },
-    ingredients: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: [0, 1000], // Maximum of 1000 characters
-      },
-    },
-    instructions: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: [0, 5000], // Maximum of 5000 characters
-      },
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "RecipeInventory",
-    tableName: "recipes",
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    static associate(models) {}
   }
-);
 
-module.exports = Recipe;
+  Recipe.init(
+    {
+      title: {
+        type: DataTypes.TEXT,
+        primaryKey: true,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      ingredients: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      instructions: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      // freezeTableName: true,
+      underscored: true,
+      modelName: "Recipe",
+      tableName: "recipes",
+    }
+  );
+  return Recipe;
+};
+
+// module.exports = Recipe;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const { RecipeInventory } = require("./models");
 const express = require("express");
 const app = express();
 const port = 4001;
-const { query } = require("./database");
+// const { query } = require("./database");
+const { Recipe } = require("./models");
+const { g } = require("./models/index");
+// const sequelize = require('../config/connection');
 require("dotenv").config();
+
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.originalUrl}`);
+  res.on("finish", () => {
+    // the 'finish' event will be emitted when the response is handed over to the OS
+    console.log(`Response Status: ${res.statusCode}`);
+  });
+  next();
+});
+app.use(express.json());
 
 app.post("/recipes", async (req, res) => {
   try {
-    const newRecipe = await RecipeInventory.create(req.body);
+    const newRecipe = await Recipe.create(req.body);
 
     res.status(201).json(newRecipe);
   } catch (err) {
@@ -16,23 +28,9 @@ app.post("/recipes", async (req, res) => {
   }
 });
 
-app.post("/recipes", async (req, res) => {
-  const RecipeData = req.body;
-  try {
-    const newRecipe = await RecipeInventory.create(RecipeData);
-    res.status(201).json(newRecipe);
-  } catch (err) {
-    if (err.name === "SequelizeValidationError") {
-      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
-    }
-    console.error(err);
-    res.status(500).json({ message: "An unexpected error occurred." });
-  }
-});
-
 app.get("/recipes", async (req, res) => {
   try {
-    const allRecipes = await RecipeInventory.findAll();
+    const allRecipes = await Recipe.findAll();
 
     res.status(200).json(allRecipes);
   } catch (err) {
@@ -41,16 +39,17 @@ app.get("/recipes", async (req, res) => {
   }
 });
 
+// Get a specific recipe
 app.get("/recipes/:id", async (req, res) => {
-  const RecipeId = parseInt(req.params.id, 10);
+  const recipeId = parseInt(req.params.id, 10);
 
   try {
-    const recipe = await RecipeInventory.findOne({ where: { id: RecipeId } });
+    const recipe = await Recipe.findOne({ where: { id: recipeId } });
 
     if (recipe) {
       res.status(200).json(recipe);
     } else {
-      res.status(404).send({ message: "Recipe not found" });
+      res.status(404).send({ message: "recipe not found" });
     }
   } catch (err) {
     console.error(err);
@@ -58,19 +57,20 @@ app.get("/recipes/:id", async (req, res) => {
   }
 });
 
+// Update a specific recipe
 app.patch("/recipes/:id", async (req, res) => {
-  const RecipeId = parseInt(req.params.id, 10);
+  const recipeId = parseInt(req.params.id, 10);
 
   try {
-    const [numberOfAffectedRows, affectedRows] = await RecipeInventory.update(
-      req.body,
-      { where: { id: RecipeId }, returning: true }
-    );
+    const [numberOfAffectedRows, affectedRows] = await Recipe.update(req.body, {
+      where: { id: recipeId },
+      returning: true,
+    });
 
     if (numberOfAffectedRows > 0) {
       res.status(200).json(affectedRows[0]);
     } else {
-      res.status(404).send({ message: "JRecipe not found" });
+      res.status(404).send({ message: "recipe not found" });
     }
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -78,16 +78,17 @@ app.patch("/recipes/:id", async (req, res) => {
   }
 });
 
+// Delete a specific recipe
 app.delete("/recipes/:id", async (req, res) => {
-  const RecipeId = parseInt(req.params.id, 10);
+  const recipeId = parseInt(req.params.id, 10);
 
   try {
-    const deleteOp = await RecipeInventory.destroy({ where: { id: RecipeId } });
+    const deleteOp = await Recipe.destroy({ where: { id: recipeId } });
 
     if (deleteOp > 0) {
-      res.status(200).send({ message: "Recipe deleted successfully" });
+      res.status(200).send({ message: "recipe deleted successfully" });
     } else {
-      res.status(404).send({ message: "Recipe not found" });
+      res.status(404).send({ message: "recipe not found" });
     }
   } catch (err) {
     console.error(err);
@@ -96,5 +97,5 @@ app.delete("/recipes/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port} .....` + g);
 });
